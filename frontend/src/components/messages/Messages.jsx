6387e19733b431c280fcea094ff5eb2e56c135e2
Shivam Kumar Singh <shivamkumar12dhr@gmail.com>
2025-01-19 14:28:19 +0530

    3rd part Completed

--- frontend/src/components/messages/Messages.jsx
+++ frontend/src/components/messages/Messages.jsx
@@ -1,34 +1,47 @@
-import React from 'react'
-import Message from './message'
-
+import React, { useEffect, useRef } from "react";
+import Message from "./message";
+import useGetMessages from "../../hooks/useGetMessages";
+import MessageSkeleton from "../skeletons/MessageSkeleton";
 
 const Messages = () => {
+  const { messages, loading } = useGetMessages();
+  const lastMessageRef = useRef();
+ 
+  
+  useEffect(() => {
+    // Scroll to the last message when messages are rendered
+    if (messages.length > 0) {
+      // Use a slight delay to ensure the DOM is ready
+      setTimeout(() => {
+        lastMessageRef.current?.scrollIntoView({ behavior: "smooth" });
+      }, 100); // Adjust the delay if needed
+    }
+  }, [messages]);
   return (
-    <div className='px-4 flex-1 overflow-auto'>
-        <Message/>
-        <Message/>
-        <Message/>
-        <Message/>
-        <Message/>
-        <Message/>
-        <Message/>
-        <Message/>
-        <Message/>
-        <Message/>
-        <Message/>
-        <Message/>
+    <div className="px-4 flex-1 overflow-auto">
+      {!loading &&
+        messages.length > 0 &&
+        messages.map((message) => (
+          <div key={message._id} ref={lastMessageRef}>
+            <Message  message={message} />
+          </div>
+        ))}
       
+
+      {loading && [...Array(3)].map((_, idx) => <MessageSkeleton key={idx} />)}
+
+      {!loading && messages.length === 0 && (
+        <p className="text-center">Send a message to start the Conversation</p>
+      )}
     </div>
-  )
-}
+  );
+};
 
-export default Messages
+export default Messages;
 
-
-// starter code 
+// starter code
 // import React from 'react'
 // import Message from './message'
-
 
 // const Messages = () => {
 //   return (
@@ -45,7 +58,7 @@ export default Messages
 //         <Message/>
 //         <Message/>
 //         <Message/>
-      
+
 //     </div>
 //   )
 // }
--- frontend/src/components/sidebar/LogoutButton.jsx
+++ frontend/src/components/sidebar/LogoutButton.jsx
@@ -1,18 +1,26 @@
-import {BiLogOut} from "react-icons/bi"
+import { BiLogOut } from "react-icons/bi";
+import useLogout from "../../hooks/useLogout";
 
 const LogoutButton = () => {
+  const { loading, logout } = useLogout();
+
   return (
     <div className="mt-auto">
-      <BiLogOut className="w-6 h-6 text-white cursor-pointer"/>
+      {!loading ? (
+        <BiLogOut
+          className="w-6 h-6 text-white cursor-pointer"
+          onClick={logout}
+        />
+      ) : (
+        <span className="loading loading-spinner"></span>
+      )}
     </div>
-  )
-}
+  );
+};
 
+export default LogoutButton;
 
-export default LogoutButton
-
-
-// starter code 
+// starter code
 // import {BiLogOut} from "react-icons/bi"
 
 // const LogoutButton = () => {
@@ -22,6 +30,5 @@ export default LogoutButton
 //     </div>
 //   )
 // }
-
 
 // export default LogoutButton
