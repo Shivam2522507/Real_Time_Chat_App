a1b1ea54dbdacd6cfd688fff2b020e7c9a218548
Shivam Kumar Singh <shivamkumar12dhr@gmail.com>
2025-01-19 17:58:34 +0530

    deployment changes

--- backend/server.js
+++ backend/server.js
@@ -1,4 +1,5 @@
 // const express = require("express");
+import path from 'path'
 import express from "express"
 // const dotenv = require("dotenv")
 import dotenv from "dotenv"
@@ -16,18 +17,22 @@ dotenv.config()
 
 const PORT = process.env.PORT || 5000;
 
+const __dirname = path.resolve()
+
 app.use(express.json())
 app.use(cookieParser())
 
-// app.get("/", (req,res)=>{
-//     // root route
-//     res.send("Hello World!")
-// })
+
 
 app.use("/api/auth", authRoutes);
 app.use("/api/messages", messageRoutes);
 app.use("/api/users", userRoutes);
 
+app.use(express.static(path.join(__dirname,"/frontend/dist")))
+
+app.get("*" , (req , res ) => {
+    res.sendFile(path.join(__dirname,"frontend","dist","index.html"))
+})
 
 server.listen(PORT, () => {
     connectToMongoDB()
